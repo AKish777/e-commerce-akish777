080c349f0b1330a2a3d9dba13bb779933d5f022e
Alex Kichigin <alexkichigin@Alexs-MacBook-Pro.local>
2021-09-25 11:28:38 -0400

    added more code and created asociations for models

--- models/index.js
+++ models/index.js
@@ -3,6 +3,25 @@ const Category = require('./Category');
 const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
+Product.belongsTo(Category, {
+    foreignKey: 'category_id',
+    onDelete: 'CASCADE',
+});
+
+Category.hasMany(Product, {
+    foreignKey: 'category_id',
+});
+  
+Product.belongsToMany(Tag, {
+    through: ProductTag,
+    foreignKey: 'project_id',
+});
+  
+Tag.belongsToMany(Product, {
+    through: ProductTag,
+    foreignKey: 'tag_id',
+});
+
 module.exports = {
   Product,
   Category,
